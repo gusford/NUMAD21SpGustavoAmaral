d3d289ad96ff86c2c62fea1c3a68bd7bb045278e
Gustavo Amaral <amaral.g@husky.neu.edu>
2021-02-08 14:55:19 -0500

    Completed UI & event handling

--- app/src/main/java/com/example/numad21sp_gustavoamaral/ButtonGridActivity.java
+++ app/src/main/java/com/example/numad21sp_gustavoamaral/ButtonGridActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Console;
 
 public class ButtonGridActivity extends AppCompatActivity {
 
@@ -16,21 +20,37 @@ public class ButtonGridActivity extends AppCompatActivity {
     public void onClick(View view)
     {
         switch (view.getId()) {
-            case R.id.buttonReturnHome:
-                break;
             case R.id.buttonA:
+                SetSelectedButton("A");
                 break;
             case R.id.buttonB:
+                SetSelectedButton("B");
                 break;
             case R.id.buttonC:
+                SetSelectedButton("C");
                 break;
             case R.id.buttonD:
+                SetSelectedButton("D");
                 break;
             case R.id.buttonE:
+                SetSelectedButton("E");
                 break;
             case R.id.buttonF:
+                SetSelectedButton("F");
+                break;
+
+            case R.id.buttonReturnHome:
+                finish();
                 break;
         }
 
+    }
+
+    private void SetSelectedButton(String buttonString)
+    {
+        TextView textView = findViewById(R.id.buttonPressedTextView);
+
+        if (textView != null)
+            textView.setText("Pressed: " + buttonString);
     }
 }
