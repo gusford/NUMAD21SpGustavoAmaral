4756c1510a365c3f87a06e4624ccd73410905eb8
Gustavo Amaral <amaral.g@husky.neu.edu>
2021-02-08 14:19:47 -0500

    Began adding event handling to buttons

--- app/src/main/java/com/example/numad21sp_gustavoamaral/ButtonGridActivity.java
+++ app/src/main/java/com/example/numad21sp_gustavoamaral/ButtonGridActivity.java
@@ -3,6 +3,7 @@ package com.example.numad21sp_gustavoamaral;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ButtonGridActivity extends AppCompatActivity {
 
@@ -10,5 +11,26 @@ public class ButtonGridActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_grid);
+    }
+
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.buttonReturnHome:
+                break;
+            case R.id.buttonA:
+                break;
+            case R.id.buttonB:
+                break;
+            case R.id.buttonC:
+                break;
+            case R.id.buttonD:
+                break;
+            case R.id.buttonE:
+                break;
+            case R.id.buttonF:
+                break;
+        }
+
     }
 }
