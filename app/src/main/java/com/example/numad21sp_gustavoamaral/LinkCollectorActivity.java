aec6cb9bfd4404a20327af0145a25fd8a525d5a6
Gustavo Amaral <amaral.g@husky.neu.edu>
2021-02-15 12:34:23 -0500

    Basic List working. TODO: Activate URL link

--- app/src/main/java/com/example/numad21sp_gustavoamaral/LinkCollectorActivity.java
+++ app/src/main/java/com/example/numad21sp_gustavoamaral/LinkCollectorActivity.java
@@ -6,11 +6,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -21,7 +18,6 @@ public class LinkCollectorActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RviewAdapter rviewAdapter;
     private RecyclerView.LayoutManager rLayoutManger;
-    private FloatingActionButton addButton;
 
     private static final String KEY_OF_INSTANCE = "KEY_OF_INSTANCE";
     private static final String NUMBER_OF_ITEMS = "NUMBER_OF_ITEMS";
@@ -31,41 +27,31 @@ public class LinkCollectorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_collector);
 
-        initializeList(savedInstanceState);
+        // Retrieve any past data
+        initListData(savedInstanceState);
 
-        addButton = findViewById(R.id.buttonAddURL);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pos = 0;
-                addItem(pos);
-            }
+        // Render the list view
+        createRecyclerView();
+
+        // Create Hook for AddURL button
+        findViewById(R.id.buttonAddURL).setOnClickListener(v -> {
+            addItem(0);
         });
-
-        //Specify what action a specific gesture performs, in this case swiping right or left deletes the entry
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                Toast.makeText(LinkCollectorActivity.this, "Delete an item", Toast.LENGTH_SHORT).show();
-                int position = viewHolder.getLayoutPosition();
-                urlCardList.remove(position);
-
-                rviewAdapter.notifyItemRemoved(position);
-
-            }
-        });
-        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 
-    void initializeList(Bundle savedInstanceState)
+    void initListData(Bundle savedInstanceState)
     {
-        // Not the first time to open this Activity
-        if (savedInstanceState != null && savedInstanceState.containsKey(NUMBER_OF_ITEMS)) {
+        // The first time this Activity is opened, show placeholder items
+        if (savedInstanceState == null || !savedInstanceState.containsKey(NUMBER_OF_ITEMS)) {
+            ItemCard item1 = new ItemCard(0, "http://www.google.com");
+            ItemCard item2 = new ItemCard(1, "http://www.amazon.com");
+            ItemCard item3 = new ItemCard(2, "http://www.chess.com");
+            urlCardList.add(item1);
+            urlCardList.add(item2);
+            urlCardList.add(item3);
+        }
+        // If it was opened before, retrieve stored items
+        else {
             if (urlCardList == null || urlCardList.size() == 0) {
 
                 int size = savedInstanceState.getInt(NUMBER_OF_ITEMS);
@@ -88,39 +74,7 @@ public class LinkCollectorActivity extends AppCompatActivity {
                 }
             }
         }
-        // The first time to open this Activity
-        else {
-            ItemCard item1 = new ItemCard(0, "http://www.google.com");
-            ItemCard item2 = new ItemCard(1, "http://www.amazon.com");
-            ItemCard item3 = new ItemCard(2, "http://www.chess.com");
-            urlCardList.add(item1);
-            urlCardList.add(item2);
-            urlCardList.add(item3);
-        }
 
-        createRecyclerView();
-    }
-
-    // Handling Orientation Changes on Android
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        // TODO: Rework this
-        int size = urlCardList == null ? 0 : urlCardList.size();
-        outState.putInt(NUMBER_OF_ITEMS, size);
-
-        // Need to generate unique key for each item
-        // This is only a possible way to do, please find your own way to generate the key
-        for (int i = 0; i < size; i++) {
-            // put image information id into instance
-            outState.putInt(KEY_OF_INSTANCE + i + "0", urlCardList.get(i).getImageSource());
-            // put itemName information into instance
-            outState.putString(KEY_OF_INSTANCE + i + "1", urlCardList.get(i).getItemName());
-            // put itemDesc information into instance
-            outState.putString(KEY_OF_INSTANCE + i + "2", urlCardList.get(i).getItemDesc());
-            // put isChecked information into instance
-            outState.putBoolean(KEY_OF_INSTANCE + i + "3", urlCardList.get(i).getStatus());
-        }
-        super.onSaveInstanceState(outState);
     }
 
     private void createRecyclerView() {
@@ -130,20 +84,36 @@ public class LinkCollectorActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
 
         rviewAdapter = new RviewAdapter(urlCardList);
-        ItemClickListener itemClickListener = new ItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                //attributions bond to the item has been changed
-                urlCardList.get(position).onItemClick(position);
-
-                rviewAdapter.notifyItemChanged(position);
-            }
-        };
-        rviewAdapter.setOnItemClickListener(itemClickListener);
+        rviewAdapter.setOnItemClickListener(position -> {
+            urlCardList.get(position).onItemClick(position);
+            rviewAdapter.notifyItemChanged(position);
+        });
 
         recyclerView.setAdapter(rviewAdapter);
         recyclerView.setLayoutManager(rLayoutManger);
     }
+
+//
+//    // TODO: Handling Orientation Changes on Android
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        int size = urlCardList == null ? 0 : urlCardList.size();
+//        outState.putInt(NUMBER_OF_ITEMS, size);
+//
+//        // Need to generate unique key for each item
+//        // This is only a possible way to do, please find your own way to generate the key
+//        for (int i = 0; i < size; i++) {
+//            // put image information id into instance
+//            outState.putInt(KEY_OF_INSTANCE + i + "0", urlCardList.get(i).getImageSource());
+//            // put itemName information into instance
+//            outState.putString(KEY_OF_INSTANCE + i + "1", urlCardList.get(i).getItemName());
+//            // put itemDesc information into instance
+//            outState.putString(KEY_OF_INSTANCE + i + "2", urlCardList.get(i).getItemDesc());
+//            // put isChecked information into instance
+//            outState.putBoolean(KEY_OF_INSTANCE + i + "3", urlCardList.get(i).getStatus());
+//        }
+//        super.onSaveInstanceState(outState);
+//    }
 
     private void addItem(int position) {
         urlCardList.add(position, new ItemCard(Math.abs(new Random().nextInt(100000)), "Input URL..."));
